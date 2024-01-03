8845e0fb5aeba9576bf6f6ef82f044914566c265
bion-slmn <bionsol25@gmail.com>
2024-01-03 21:31:52 +0000

    final 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,8 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((response) => console.log(response));
+    .then((responses) => responses.map((val) => ({
+      status: val.status,
+      value: val.value !== undefined ? val.value : val.reason,
+    })));
 }
