54a097d44b0b06f683271b9c537bf9767b06e8ff
bion-slmn <bionsol25@gmail.com>
2024-01-03 20:29:41 +0000

    using promise.all method

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const [promise1, promise2] = [uploadPhoto(), createUser()];
 
-  Promise.all([promise1, promise2])
+  return Promise.all([promise1, promise2])
     .then((responses) => {
       const [response1, response2] = responses;
       console.log(`${response1.body} ${response2.firstName} ${response2.lastName}`);
