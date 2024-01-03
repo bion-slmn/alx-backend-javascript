88951d6ece3522faaa74962929a1975d0f061002
bion-slmn <bionsol25@gmail.com>
2024-01-03 10:14:47 +0000

    for of loops changing array

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const array1 = [...array];
-  for (const [index, value] of array1.entries()) {
-    array1[index] = `${appendString}${value}`;
-  }
 
-  return array1;
+  for (const [index, value] of array.entries()) {
+    array[index] = `${appendString}${value}`;
+  }
+  return array;
 }
