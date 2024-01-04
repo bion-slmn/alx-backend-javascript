241ca18455ce3895c7d2c3f281afc6fcc9381bf0
bion-slmn <bionsol25@gmail.com>
2024-01-04 12:05:24 +0000

    class objects created

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -3,6 +3,6 @@ import ClassRoom from './0-classroom';
 export default function initializeRooms() {
   const ob = new ClassRoom(19);
   const ob1 = new ClassRoom(20);
-  const ob2 = new ClassRoom(24);
+  const ob2 = new ClassRoom(34);
   return [ob, ob1, ob2];
 }
