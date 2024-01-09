3c68af7425dfe156081a407e0f50342887327033
bion-slmn <bionsol25@gmail.com>
2024-01-09 06:02:36 +0000

    return an array of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,13 @@
+/*
+ * getListStudents - get alist of students
+ * @param {nothing} - doesnt no take in any param
+ * @return {list}  return alist of students
+ */
+
+export default function getListStudents() {
+  return [
+    { firstName: 'Guillaume', id: 1, location: 'San Francisco' },
+    { firstName: 'James', id: 2, location: 'Columbia' },
+    { firstName: 'Serena', id: 5, location: 'San Francisco' },
+  ];
+}
