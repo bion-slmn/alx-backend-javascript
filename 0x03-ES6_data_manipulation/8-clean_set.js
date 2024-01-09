ab745726d03a51ae352c1376750e6a5eea50c13e
bion-slmn <bionsol25@gmail.com>
2024-01-09 07:33:30 +0000

    cleaning a set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -6,13 +6,11 @@
  */
 export default function cleanSet(set, startString) {
   // creat an arrayi
-  if (startString === '') return '';
-  const arr = Array.from(set);
-  const startArr = arr.filter((str) => str.startsWith(startString));
-
-  if (startArr.lenght !== 0) {
-    const results = startArr.map((str) => str.slice(startString.length));
-    return results.join('-');
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
   }
-  return '';
+  return Array.from(set)
+    .filter((str) => str.startsWith(startString))
+    .map((str) => str.slice(startString.length))
+    .join('-');
 }
