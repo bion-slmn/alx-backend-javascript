b7bef15f1614bdf21a34702dda66ff0a609c34b5
bion-slmn <bionsol25@gmail.com>
2024-02-27 20:47:22 +0000

    an express server

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,66 @@
+/*
+ * create a server using express
+ */
+const express = require('express');
+const args = require('process').argv;
+const fs = require('fs');
+
+const app = express();
+
+function countStudents(filePath) {
+  return new Promise((resolve, reject) => {
+    if (!fs.existsSync(filePath) || !fs.statSync(filePath).isFile()) {
+      reject(new Error('Cannot load the database'));
+    }
+
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      const lines = data.trim().split('\n');
+      const removeHeading = lines.slice(1);
+
+      const studentNo = removeHeading.length;
+      const records = {};
+      const results = [];
+
+      removeHeading.forEach((val) => {
+        const recordArray = val.split(',');
+        const field = recordArray[recordArray.length - 1];
+
+        if (!Object.keys(records).includes(field)) {
+          records[field] = [recordArray[0]];
+        } else {
+          records[field].push(recordArray[0]);
+        }
+      });
+
+      results.push(`Number of students: ${studentNo}`);
+      Object.keys(records).forEach((key) => {
+        const value = records[key];
+        results.push(`Number of students in ${key}: ${
+          value.length}. List: ${value.join(', ')}`);
+      });
+      resolve(results);
+    });
+  });
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  countStudents(args[2])
+    .then((result) => {
+      res.send(`This is the list of our students:\n${result.join('\n')}`);
+    })
+    .catch((error) => {
+      res.send(error.message);
+    });
+});
+
+app.listen(1245);
+
+module.exports = app;
