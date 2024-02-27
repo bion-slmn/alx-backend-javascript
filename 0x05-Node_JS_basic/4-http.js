6562d3c9bbbfc283208538c429e39f2e8120c1ef
bion-slmn <bionsol25@gmail.com>
2024-02-27 19:43:39 +0000

    adding s small server

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,10 @@
+/* create a small server
+ */
+const http = require('http');
+
+const app = http.createServer((request, response) => {
+  response.write('Hello Holberton School!');
+  response.end();
+}).listen(1245);
+
+module.exports = app;
